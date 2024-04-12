b31d9a0030eb3222e86a827e3cdfa18965fe55d9
bolgianialberto <bolgibasso@LAPTOP-GNS7GATQ>
2024-04-12 14:04:40 +0200

    Added AnnotationTopBar

---
+++ ui/src/components/common/AnnotationTopBar.tsx
@@ -0,0 +1,10 @@
+import { MdLogout, MdOutlineHouse } from 'react-icons/md';
+
+export type AnnotationTopbarProps = {
+    height: string;
+    leftOffset?: string;
+};
+
+const AnnotationTopBar: React.FC<AnnotationTopbarProps> = ({ height, leftOffset})
+
+
