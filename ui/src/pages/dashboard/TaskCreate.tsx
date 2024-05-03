b7735b6482d9bb38b63a7c3db736fe31c63051d3
youssef.elfadi <youssef.yef91@gmail.com>
2024-05-03 15:32:06 +0200

    Changed output error in form "Task create"
    Now the errors are displayed under the input section
    where the error occurs.

--- ui/src/pages/dashboard/TaskCreate.tsx
+++ ui/src/pages/dashboard/TaskCreate.tsx
@@ -1,7 +1,7 @@
 import { MdAddTask } from 'react-icons/md';
 import styled from 'styled-components';
 import { FormEvent, useEffect, useRef, useState } from 'react';
-import { Header, Input, InputType, Option, Table } from '../../components/common';
+import { Header, Input, InputError, InputType, Option, Table } from '../../components/common';
 import { Doc, User, getApiError, useDocumentApi, useTaskApi, useUserApi, TaskExtended, TaskStatus } from '../../api';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Select from "react-select";
@@ -30,7 +30,14 @@ const TaskCreatePage = () => {
     const location = useLocation();
     const navigate = useNavigate();
 
-    const [errorMsg, setErrorMsg] = useState<string>('');
+    const [errorMsgApi, setErrorMsgApi] = useState<string>('');
+    const [errorMsg, setErrorMsg] = useState<any>({
+        user: null,
+        document: null,
+        description: null,
+        startPage: null, 
+        endPage: null,
+    });
     const errorRef = useRef<HTMLParagraphElement>(null);
 
     useEffect(() => {
@@ -94,42 +101,55 @@ const TaskCreatePage = () => {
     const onCreateTask = async (e: FormEvent) => {
         e.preventDefault();
 
+        const errors = {};
+
         if (!userOption) {
-            setErrorMsg('A user must be selected!');
-            return;
+            errors.user = 'A user must be selected!';
+            // setErrorMsg('A user must be selected!');
+            // return;
         }
         if (!docOption) {
-            setErrorMsg('A document must be selected!');
-            return;
+            errors.document = 'A document must be selected!';
+            // setErrorMsg('A document must be selected!');
+            // return;
         }
         if (description.trim().length === 0) {
-            setErrorMsg('A description must be specified!');
-            return;
+            errors.description = 'A description must be specified!'
+            // setErrorMsg('A description must be specified!');
+            // return;
         }
-        if (!startPage || !endPage) {
-            setErrorMsg('Starting and ending page must be declared.');
-            return;
+        if (!startPage) {
+            errors.startPage = "Starting page must be selected.";
+            // setErrorMsg('Starting and ending page must be declared.');
+            // return;
+        }
+        if (!endPage) {
+            errors.endPage = 'Ending page must be selected';
         }
 
-        const newTask = {
-            userId: userOption.value._id,
-            docId: docOption.value._id,
-            pageRange: {
-                start: startPage,
-                end: endPage,
-            },
-            description: description,
-        };
+        setErrorMsg(errors);
 
-        createTask(newTask)
-            .then((task) => {
-                navigate(`/dash/tasks/${task._id}`);
-                notification.success({
-                    message: 'Task created succesfully!',
-                    placement: 'bottomRight',
-                });
-            })
-            .catch((err) => setErrorMsg(getApiError(err)));
+        if (Object.keys(errors).length === 0) {
+            const newTask = {
+                userId: userOption.value._id,
+                docId: docOption.value._id,
+                pageRange: {
+                    start: startPage,
+                    end: endPage,
+                },
+                description: description,
+            };
+
+            createTask(newTask)
+                .then((task) => {
+                    navigate(`/dash/tasks/${task._id}`);
+                    notification.success({
+                        message: 'Task created succesfully!',
+                        placement: 'bottomRight',
+                    });
+                })
+                .catch((err) => setErrorMsgApi(getApiError(err)));
+        }
     };
 
     const selectDoc = (docOption: any) => {
@@ -149,9 +169,9 @@ const TaskCreatePage = () => {
             </Header>
 
             <Form>
-                {errorMsg && (
+                {errorMsgApi && (
                     <p className="errorMsg" ref={errorRef}>
-                        {errorMsg}
+                        {errorMsgApi}
                     </p>
                 )}
 
@@ -164,7 +184,41 @@ const TaskCreatePage = () => {
                         gap: '25px',
                         width:"100%"
                     }}>
-                    <InputWrapper> 
+                    <InputWrapper>
+                    {errorMsg.user ?
+                        <ErrorWrapper>
+                            <Select
+                                styles={{
+                                    control: (provided) => ({
+                                        ...provided,
+                                        width: '100%',
+                                        boxShadow: 'none',
+                                        borderColor: 'red', // Imposta il colore del bordo a rosso
+                                        '&:hover': {
+                                            borderColor: 'red' // Anche quando si passa sopra con il mouse
+                                        },
+                                        '&:focus': {
+                                            borderColor: 'red' // Anche quando il select è in stato di focus
+                                        }
+                                    }),
+                                    container: base => ({
+                                        ...base,
+                                        flex: 1,
+                                    }),
+                                    singleValue: provided => ({
+                                        ...provided,
+                                        borderColor: 'red'
+                                    })
+                                }}
+                                placeholder="Select User"
+                                options={userOptions}
+                                value={userOption}
+                                onChange={(user) => setUserOption(user)}
+                                isSearchable
+                            />
+                            <span style={{color: "red"}}>{errorMsg.user}</span>
+                        </ErrorWrapper>
+                        :
                         <Select
                             styles={{
                                 control: (provided) => ({ ...provided, width: '100%' }),
@@ -179,6 +233,41 @@ const TaskCreatePage = () => {
                             onChange={(user) => setUserOption(user)}
                             isSearchable
                         />
+                    }
+                    {errorMsg.document ?
+                        <ErrorWrapper>
+                            <Select
+                                styles={{
+                                    control: (provided) => ({
+                                        ...provided,
+                                        width: '100%',
+                                        boxShadow: 'none',
+                                        borderColor: 'red', // Imposta il colore del bordo a rosso
+                                        '&:hover': {
+                                            borderColor: 'red' // Anche quando si passa sopra con il mouse
+                                        },
+                                        '&:focus': {
+                                            borderColor: 'red' // Anche quando il select è in stato di focus
+                                        }
+                                    }),
+                                    container: base => ({
+                                        ...base,
+                                        flex: 1,
+                                    }),
+                                    singleValue: provided => ({
+                                        ...provided,
+                                        borderColor: 'red'
+                                    })
+                                }}
+                                placeholder="Select Document"
+                                options={docOptions}
+                                value={docOption}
+                                onChange={(doc) => selectDoc(doc)}
+                                isSearchable
+                            />
+                            <span style={{color: "red"}}>{errorMsg.document}</span>
+                        </ErrorWrapper>
+                        :
                         <Select
                             styles={{
                                 control: (provided) => ({ ...provided, width: '100%' }),
@@ -193,21 +282,58 @@ const TaskCreatePage = () => {
                             onChange={(doc) => selectDoc(doc)}
                             isSearchable
                         />
+                    }
                     </InputWrapper>
                 </div>
-
-                <Input
-                    type="text"
-                    variant={InputType.STANDARD}
-                    id="description"
-                    placeholder="Task Description"
-                    onChange={(e) => setDescription(e.target.value)}
-                    value={description}
-                    width="100%"
-                    required
-                />
+                
+                <InputWrapper>
+                    {errorMsg.description ?
+                        <ErrorWrapperSingleComponent>
+                            <InputError
+                                type="text"
+                                variant={InputType.STANDARD}
+                                id="description"
+                                placeholder="Task Description"
+                                onChange={(e) => setDescription(e.target.value)}
+                                value={description}
+                                width="100%"
+                                required
+                            />
+                            <span style={{color: "red"}}>{errorMsg.description}</span>
+                        </ErrorWrapperSingleComponent>
+                    :
+                        <Input
+                            type="text"
+                            variant={InputType.STANDARD}
+                            id="description"
+                            placeholder="Task Description"
+                            onChange={(e) => setDescription(e.target.value)}
+                            value={description}
+                            width="100%"
+                            required
+                        />
+                    }
+                </InputWrapper>
+                
                 {docOption && (
-                    <InputWrapper>         
+                    <InputWrapper>
+                        {errorMsg.startPage ?
+                            <ErrorWrapperSingleComponent>
+                                <InputError
+                                    type="number"
+                                    min="1"
+                                    max={docOption.totalPages}
+                                    variant={InputType.STANDARD}
+                                    id="startPage"
+                                    placeholder="Start Page"
+                                    onChange={(e) => setStartPage(Number(e.target.value))}
+                                    value={startPage}
+                                    width="100%"
+                                    required
+                                />
+                                <span style={{color: "red"}}>{errorMsg.startPage}</span>
+                            </ErrorWrapperSingleComponent>
+                        :
                         <Input
                             type="number"
                             min="1"
@@ -220,7 +346,24 @@ const TaskCreatePage = () => {
                             width="100%"
                             required
                         />
-
+                        }          
+                        {errorMsg.endPage ?
+                            <ErrorWrapperSingleComponent>
+                                <InputError
+                                    type="number"
+                                    min="1"
+                                    max={docOption.totalPages}
+                                    variant={InputType.STANDARD}
+                                    id="endPage"
+                                    placeholder={`Final Page - max=${docOption.totalPages}`}
+                                    onChange={(e) => setEndPage(Number(e.target.value))}
+                                    value={endPage}
+                                    width="100%"
+                                    required
+                                />
+                                <span style={{color: "red"}}>{errorMsg.endPage}</span>
+                            </ErrorWrapperSingleComponent>
+                        :
                         <Input
                             type="number"
                             min="1"
@@ -233,6 +376,7 @@ const TaskCreatePage = () => {
                             width="100%"
                             required
                         />
+                        }   
                     </InputWrapper>
                 )}
 
@@ -346,8 +490,7 @@ const InputWrapper = styled.div`
     width: 100%;
     display: flex;
     flex-direction: row;
-    gap: 10px; // Spazio tra gli input
-    align-items: center;
+    gap: 10px;
 `;
 
 const TableWrapper = styled.div`
@@ -358,4 +501,18 @@ const TableWrapper = styled.div`
     align-items: center;
     flex-direction: column;
     gap: 25px;
+`;
+
+const ErrorWrapper = styled.div`
+    width: 50%;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+`;
+
+const ErrorWrapperSingleComponent = styled.div`
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
 `;
